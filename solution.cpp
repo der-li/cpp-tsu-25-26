08841a923984ab3cfee935c8b60dc605e9ea2baa
der-li <lizameka222@gmail.com>
2025-11-21 19:43:26 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -2,54 +2,67 @@
 #include <string>
 using namespace std;
 
+struct Node {
+    long long data;   
+    Node* next_node;  
+    Node(long long val): data(val), next_node(nullptr) {}
+};
+
 struct List {
-    long long value;   
-    List* next;       
-    List* current;        
+    Node* head;     
+    Node* current_node;
 
-    List(long long v) : value(v), next(nullptr), current(this) {}
+    List() {
+        head = new Node(100); 
+        current_node = head;
+    }
 
-    void append(long long x) {
-        List* newNode = new List(x);
-        newNode->next = current->next;
-        current->next = newNode;
-        current = newNode;
+    void append(long long X) {
+        Node* new_node = new Node(X);        
+        new_node->next_node = current_node->next_node; 
+        current_node->next_node = new_node;
+        current_node = new_node;             
     }
 
     void go(long long steps) {
-        while (steps > 0 && current->next != nullptr) {
-            current = current->next;
+        while (steps > 0 && current_node->next_node != nullptr) {
+            current_node = current_node->next_node;
             steps--;
         }
     }
 
-    long long getValue() {
-        return current->value;
+    void print() {
+        cout << current_node->data;
     }
 
     void reset() {
-        current = this;
+        current_node = head; 
     }
 };
 
 int main() {
-    List list(100); 
+    List list; 
+    long long last_value = 100;
+    long long current_value = 100; 
+    string cmd;
 
-    string command;
-    while (cin >> command) {
-        if (command == "append") {
+    while (cin >> cmd) {
+        if (cmd == "append") {
             long long X; cin >> X;
-            list.append(X); 
-            cout << "append " << X << " - OK\n";
-        } else if (command == "go") {
-            long long N; cin >> N;
+            list.append(X);
+            last_value = current_value = X;
+            cout << "append " << X << " - OK" << '\n';
+        } else if (cmd == "go") {
+            long long N; cin >> N; long long originalN = N;
+            current_value = last_value; 
             list.go(N);
-            cout << "go " << N << " - OK\n";
-        } else if (command == "print") {
-            cout << "print " << list.getValue() << " - OK\n";
-        } else if (command == "reset") {
-            list.reset(); 
-            cout << "reset - OK\n";
+            cout << "go " << originalN << " - OK" << '\n';
+        } else if (cmd == "print") {
+            cout << "print " << list.print() << " - OK" << '\n';
+        } else if (cmd == "reset") {
+            current_value = 100; 
+            list.reset();
+            cout << "reset - OK" << '\n';
         }
     }
     return 0;

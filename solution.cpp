19b0e8276ced50b73cd4e1374678861dd0de9e12
der-li <lizameka222@gmail.com>
2025-11-21 20:13:28 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -3,54 +3,60 @@
 using namespace std;
 
 struct Node {
-    long long data;    
-    Node* next_node;    
-    Node(long long val) : data(val), next_node(nullptr) {}
+    long long value; 
+    Node* next;     
+
+    Node(long long val): value(val), next(nullptr) {}
 };
 
 struct List {
-    Node* head;      
+    Node* first;     
     Node* current;       
 
     List() {
-        head = new Node(100); 
-        current = head;
+        first = new Node(100);
+        current = first;     
     }
 
     void append(long long X) {
-        Node* new_node = new Node(X);       
-        new_node->next_node = current->next_node; 
-        current->next_node = new_node;
-        current = new_node;             
+        Node* new_node = new Node(X);
+        new_node->next = current->next;
+        current->next = new_node;
+        current = new_node; 
     }
 
     void go(long long N) {
-        while (N > 0 && current->next_node != nullptr) {
-            current = current->next_node;
+        while (N > 0 && current->next != nullptr) {
+            current = current->next;
             N--;
         }
     }
 
     void print() {
-        cout << current->data; 
+        cout << current->value;
     }
 
     void reset() {
-        current = head;
+        current = first;
     }
 };
 
 int main() {
-    List list; 
+    List list;
+
+    long long last_value = 100; 
+    long long current_value = 100; 
     string cmd;
 
     while (cin >> cmd) {
         if (cmd == "append") {
             long long X; cin >> X;
             list.append(X);
+            last_value = current_value = X; 
             cout << "append " << X << " - OK" << '\n';
         } else if (cmd == "go") {
             long long N; cin >> N;
+            current_value = last_value; 
             list.go(N);
             cout << "go " << N << " - OK" << '\n';
         } else if (cmd == "print") {
@@ -59,8 +65,10 @@ int main() {
             cout << " - OK" << '\n';
         } else if (cmd == "reset") {
             list.reset();
+            current_value = 100; 
             cout << "reset - OK" << '\n';
         }
     }
+
     return 0;
 }

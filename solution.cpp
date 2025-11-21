997c285f87b5042d99282fd7b1a3006e0a2e9059
der-li <lizameka222@gmail.com>
2025-11-21 19:38:26 +0300

    Update solution.cpp

--- solution.cpp
+++ solution.cpp
@@ -1,62 +1,55 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
-struct Node {
-    long long value;
-    Node* next;
-    Node(long long val): value(val), next(nullptr) {}
-};
-
 struct List {
-    Node* first;  
-    Node* current;  
-    List() {
-        first = new Node(100);
-        current = first;
-    }
+    long long value;   
+    List* next;       
+    List* current;        
 
-    void append(long long X) {
-        Node* newNode = new Node(X);
+    List(long long v) : value(v), next(nullptr), current(this) {}
+
+    void append(long long x) {
+        List* newNode = new List(x);
         newNode->next = current->next;
         current->next = newNode;
         current = newNode;
     }
 
-    void go(long long N) {
-        while (current->next != nullptr && N > 0) {
+    void go(long long steps) {
+        while (steps > 0 && current->next != nullptr) {
             current = current->next;
-            N--;
+            steps--;
         }
     }
 
-    long long get() {
+    long long getValue() {
         return current->value;
     }
 
     void reset() {
-        current = first;
+        current = this;
     }
 };
 
 int main() {
-    List list;
-    std::string cmd;
-    while (cin >> cmd) {
-        if (cmd == "append") {
+    List list(100); 
+
+    string command;
+    while (cin >> command) {
+        if (command == "append") {
             long long X; cin >> X;
-            list.append(X);
-            cout << "append " << X << " - OK" << '\n';
-        } else if (cmd == "go") {
+            list.append(X); 
+            cout << "append " << X << " - OK\n";
+        } else if (command == "go") {
             long long N; cin >> N;
             list.go(N);
-            cout << "go " << N << " - OK" << '\n';
-        } else if (cmd == "print") {
-            cout << "print " << list.get() << " - OK" << '\n';
-        } else if (cmd == "reset") {
-            list.reset();
-            cout << "reset - OK" << '\n';
+            cout << "go " << N << " - OK\n";
+        } else if (command == "print") {
+            cout << "print " << list.getValue() << " - OK\n";
+        } else if (command == "reset") {
+            list.reset(); 
+            cout << "reset - OK\n";
         }
     }
     return 0;
